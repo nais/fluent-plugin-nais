e4de6ae9985ab69ced562319739faa290f211392
Terje Sannum <terje.sannum@nav.no>
2017-10-04 13:25:49 +0200

    more generic prefix_fields filter

--- lib/fluent/plugin/filter_nais_prefix_fields.rb
+++ lib/fluent/plugin/filter_nais_prefix_fields.rb
@@ -4,14 +4,18 @@ require 'nais/log/parser'
 
 module Fluent::Plugin
   class NaisPrefixFilter < Filter
-    Fluent::Plugin.register_filter('nais_prefix_nonstandard', self)
-      
+    Fluent::Plugin.register_filter('nais_prefix_fields', self)
+    config_param :prefix, :string
+    config_param :regex, :string
+    
+    
     def configure(conf)
       super
+      @re = Regexp.new(@regex)
     end
       
     def filter(tag, time, record)
-      ::Nais::Log::Parser.prefix_nonstandard_fields(record)
+      ::Nais::Log::Parser.prefix_fields(record, @prefix, @re)
     end
     
   end
