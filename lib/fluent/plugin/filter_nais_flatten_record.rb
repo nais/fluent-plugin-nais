be18e67d44ae630b0c8e3eb241631cd5f59a7107
Terje Sannum <terje.sannum@nav.no>
2018-06-14 13:06:18 +0200

    added option to keep some fields unchanged

--- lib/fluent/plugin/filter_nais_flatten_record.rb
+++ lib/fluent/plugin/filter_nais_flatten_record.rb
@@ -5,13 +5,15 @@ require 'nais/log/parser'
 module Fluent::Plugin
   class NaisFlattenFilter < Filter
     Fluent::Plugin.register_filter('nais_flatten_record', self)
-      
+    config_param :keep, :string, default: ""
+
     def configure(conf)
       super
+      @re = @keep == "" ? nil : Regexp.new(@keep)
     end
       
     def filter(tag, time, record)
-      ::Nais::Log::Parser.flatten_hash(record)
+      ::Nais::Log::Parser.flatten_hash(record, "", @re)
     end
     
   end
