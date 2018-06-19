fd6ab5bcaa2045ef86d2f37668938941f9f96ef6
Terje Sannum <terje.sannum@nav.no>
2018-06-19 09:43:07 +0200

    added filter nais_remap_traefik

---
+++ lib/fluent/plugin/filter_nais_remap_traefik.rb
@@ -0,0 +1,17 @@
+require "fluent/plugin/nais/version"
+require 'fluent/plugin/filter'
+require 'nais/log/parser'
+
+module Fluent
+  module Plugin
+    class NaisRemapTraefikFilter < Fluent::Plugin::Filter
+      Fluent::Plugin.register_filter('nais_remap_traefik', self)
+      
+      def filter(tag, time, record)
+        record['message'] = record.delete('RequestLine') if record.has_key?('RequestLine')
+        record.delete('msg') if record['msg'] == ""
+      end
+
+    end
+  end
+end
