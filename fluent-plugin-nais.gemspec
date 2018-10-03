922be1b8885edcdaf001da0a4a9af8047cca077f
Terje Sannum <terje.sannum@nav.no>
2018-10-03 14:52:52 +0200

    support pod labels

--- fluent-plugin-nais.gemspec
+++ fluent-plugin-nais.gemspec
@@ -20,8 +20,8 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency 'fluentd', '~> 1.1'
-  spec.add_dependency 'nais-log-parser', '>= 0.30.0'
+  spec.add_dependency 'fluentd', '~> 1.2'
+  spec.add_dependency 'nais-log-parser', '>= 0.31.0'
 
   spec.add_development_dependency "bundler", "~> 1.15"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/fluent/plugin/filter_nais_remap_kubernetes.rb
+++ lib/fluent/plugin/filter_nais_remap_kubernetes.rb
@@ -1,17 +1,36 @@
 require "fluent/plugin/nais/version"
 require 'fluent/plugin/filter'
-require 'nais/log/parser'
 
 module Fluent::Plugin
   class NaisRemapKubernetesFilter < Filter
     Fluent::Plugin.register_filter('nais_remap_kubernetes', self)
+    config_param :labels, :string, default: ""
       
     def configure(conf)
       super
     end
       
     def filter(tag, time, record)
-      ::Nais::Log::Parser.remap_kubernetes_fields(record)
+      record["category"] = record.delete("stream") if record.has_key?("stream")
+      if record["docker"].is_a?(Hash)
+        record["container"] = record["docker"]["container_id"]
+        record.delete("docker")
+      end
+      if record["kubernetes"].is_a?(Hash)
+        record["host"] = record["kubernetes"]["host"]
+        record["namespace"] = record["kubernetes"]["namespace_name"]
+        record["application"] = record["kubernetes"]["container_name"]
+        record["pod"] = record["kubernetes"]["pod_name"]
+        if record["kubernetes"]["labels"].is_a?(Hash)
+          unless @labels.nil? || @labels == ""
+            @labels.split(',').each {|label|
+              record[label] = record["kubernetes"]["labels"][label] if record["kubernetes"]["labels"].has_key?(label)
+            }
+          end
+        end
+        record.delete("kubernetes")
+      end
+      record
     end
     
   end
